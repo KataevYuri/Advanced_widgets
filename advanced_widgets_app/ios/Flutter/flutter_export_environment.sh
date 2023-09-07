0b08dd0d8a43c7d2d1b33e044ee3605d51750aa6
KataevYuri <107112301+KataevYuri@users.noreply.github.com>
2023-09-07 23:26:26 +0800

    1

--- advanced_widgets_app/ios/Flutter/flutter_export_environment.sh
+++ advanced_widgets_app/ios/Flutter/flutter_export_environment.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\Src\flutter"
+export "FLUTTER_ROOT=C:\src\flutter"
 export "FLUTTER_APPLICATION_PATH=C:\GitHub\Advanced_widgets\advanced_widgets_app"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
